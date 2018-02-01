8cc10b530ac2512c892668b4234a2b4f350e8ffc
sanketsingh24 <sanketsingh24@gmail.com>
2018-02-02 00:26:11 +0530

    Everything working, time to link frontend

--- src/components/CoursePage/Semcourses.js
+++ src/components/CoursePage/Semcourses.js
@@ -3,12 +3,10 @@ import PropTypes from 'prop-types';
 import { fetchSubjectsIfNeeded } from "../../actions/action";
 import Courses from '../courses/courses';
 import Header from '../Header/Header';
-import Tabs from 'muicss/lib/react/tabs';
-import Tab from 'muicss/lib/react/tab';
 
 export default class Semcourses extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   componentDidMount() {
@@ -18,6 +16,7 @@ export default class Semcourses extends Component {
     dispatch(fetchSubjectsIfNeeded(ID, dept[ID].course_ids));
   }
 
+
   // look(courseId, i) {
   //   let semno = courseId.toString()[2];
   //   return(
@@ -26,12 +25,23 @@ export default class Semcourses extends Component {
   //         </Tab>
   //   );
   // }
+  book () {
+    if(typeof this.props.subjects == 'undefined') {
+      console.log("NO sub");
+      return (<p> NO </p>);
+    }
+    console.log("Subs here")
+    return (<p> {this.props.subjects.map( (subject) => 
+      <div>{subject.course_name}</div>
+     )} 
+    </p>);
+  }
 
   render(){
     return(
       <div>
         <Header message='List of Courses' />
-
+        {this.book()}
       </div>
     );
   }
@@ -39,4 +49,5 @@ export default class Semcourses extends Component {
 
 /**        <Tabs className="courses">
           {this.props.subjects.course_ids.map(this.look.bind(this))}
-        </Tabs> */
+        </Tabs>
+                {this.book(this.props.isFetching)} */
--- src/components/courses/courses.js
+++ src/components/courses/courses.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Courses extends Component {
-
+  constructor(props) {
+    super(props)
+  }
 
   render() {
     return (
       <div className="courses">
+        <p>Course Name: {this.props.subject.course_name} </p> 
 
       </div>
     );
